115c28a321649977887e136445321a4b9e9e4035
Eduardo Vinicius <eduardoviniciusjorge@gmail.com>
2026-07-09 23:52:07 -0300

    refactor(api/auth): add auth constants

---
+++ src/PlanejadorCompras.API/Security/AuthenticationConstants.cs
@@ -0,0 +1,8 @@
+namespace PlanejadorCompras.API.Security;
+
+internal static class AuthenticationConstants
+{
+    public const string AccessTokenCookieName = "access_token";
+    public const string XmlHttpRequestHeaderName = "X-Requested-With";
+    public const string XmlHttpRequestHeaderValue = "XmlHttpRequest";
+}
